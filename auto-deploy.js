1083dbb6035561dd92c989bf4f7956617f0b8bb2
hahaha-helpme <43705134+hahaha-helpme@users.noreply.github.com>
2025-07-04 19:52:03 +0000

    Auto update 🚀

--- auto-deploy.js
+++ auto-deploy.js
@@ -1,9 +1,8 @@
 const { exec } = require("child_process");
-
-exec("git add . && git commit -m 'Auto update' && git push", (err, stdout, stderr) => {
-  if (err) {
-    console.log("🔁 Geen wijzigingen om te committen.");
-  } else {
-    console.log("✅ Gepusht:\n", stdout);
-  }
-});
+exec(
+  `git add -A &&
+   git diff --cached --quiet || 
+   git commit -m "Auto update 🚀" &&
+   git push`,
+  (_, out) => console.log("✅ Geüpdatet!\n" + out)
+);
