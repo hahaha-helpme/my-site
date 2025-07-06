08161e3b3fd951f3c23e52265bdaaed89b460217
hahaha-helpme <43705134+hahaha-helpme@users.noreply.github.com>
2025-07-06 18:42:54 +0000

    Auto update 🚀

--- script/image-renderer.js
+++ script/image-renderer.js
@@ -1,5 +1,23 @@
 /* Beschikbare functionaliteit: ImageRenderer (AG Grid cellRenderer voor afbeeldingen) */
 (function () {
+  const style = document.createElement('style');
+  style.textContent = `
+    /* Cel centreren */
+    .ag-cell.image-cell {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+    }
+    /* Afbeelding schalen */
+    .ag-cell.image-cell img {
+      width: 35px;
+      height: 35px;
+      object-fit: contain;
+    }
+  `;
+  document.head.appendChild(style);
+
   class ImageRenderer {
     init(params) {
       this.eGui = document.createElement('img');
