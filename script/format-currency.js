392bbe62b4fda07c6fcbb6f5d2b242274745f23d
hahaha-helpme <43705134+hahaha-helpme@users.noreply.github.com>
2025-07-27 09:19:42 +0000

    Auto update 🚀

--- script/format-currency.js
+++ script/format-currency.js
@@ -1,16 +1,3 @@
-// /* Beschikbare functionaliteit: formatEuro(value) – €-notatie met twee decimalen */
-// (function () {
-//   window.formatEuro = function (value) {
-//     if (value === null || value === undefined) return '';
-//     return new Intl.NumberFormat('en-US', {
-//       style: 'currency',
-//       currency: 'EUR',
-//       minimumFractionDigits: 2,
-//       maximumFractionDigits: 2
-//     }).format(value).replace('.', ',');
-//   };
-// })();
-
 (function () {
   const currencies = {
     sk: 'EUR',
