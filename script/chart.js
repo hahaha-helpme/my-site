8ea79697c5f2b4ca82eca41363c6af496c21b13f
hahaha-helpme <43705134+hahaha-helpme@users.noreply.github.com>
2025-08-01 09:21:28 +0000

    Auto update 🚀

--- script/chart.js
+++ script/chart.js
@@ -9,7 +9,7 @@
       .chart-popup-title { font-weight: 600; font-size: 16px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }
       .chart-popup-close { cursor: pointer; border: none; background: none; font-size: 24px; font-weight: bold; color: #555; line-height: 1; }
       .chart-popup-close:hover { color: black; }
-      .chart-popup-controls { padding: 0px 20px; display: flex; gap: 16px; background-color: #f2f2f2; }
+      .chart-popup-controls { padding: 6px 20px 0px 20px; display: flex; gap: 16px; background-color: #f2f2f2; }
       .chart-popup-controls select { font-family: "Inter", sans-serif; padding: 6px 10px; border-radius: 4px; border: 1px solid #ccc; background-color: white; }
       .chart-popup-content { flex-grow: 1; padding: 20px; }`;
     document.head.appendChild(style);
