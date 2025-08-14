a0008700e46f781f6bf3812073a94a9dc363bddb
hahaha-helpme <43705134+hahaha-helpme@users.noreply.github.com>
2025-08-14 11:31:59 +0000

    Auto update 🚀

--- script/image-renderer.js
+++ script/image-renderer.js
@@ -23,11 +23,11 @@
 // }
 
 
-// .ag-cell.image-cell img {
-//   width: 35px;
-//   height: 35px;
-//   object-fit: contain; 
-// }
+.ag-cell.image-cell img {
+  width: 35px;
+  height: 35px;
+  object-fit: contain; 
+}
   `;
 
 
